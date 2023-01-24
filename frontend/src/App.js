ef1795b09fec606b262caa568cb725959de7bb70
Abdul5050 <tacticshustle@gmail.com>
2023-01-24 08:24:38 +0100

    Added trending jobs page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,6 +30,7 @@ import IndicatorDetails from './components/pages/IndicatorDetails';
 import IndicatorDetailsMeta from './components/pages/IndicatorDetailsMeta';
 import IndicatorDetailsMetaChart from './components/pages/IndicatorDetailsMetaCharts';
 import KeyFacts from './pages/keyfacts';
+import ExternalJobs from './pages/ExternalJobs';
 
 
 
@@ -65,6 +66,7 @@ function App() {
           <Route exact path="/trends" component={Trends} />
           <Route exact path="/PrivacyPolicy"  component={PrivacyPolicy} />
           <Route exact path="/key-facts" component={KeyFacts} />
+          <Route exact path="/trending-jobs" component={ExternalJobs} />
         </Switch>
         <Footer/>
       </>
