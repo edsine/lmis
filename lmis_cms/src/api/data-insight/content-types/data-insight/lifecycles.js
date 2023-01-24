02f67ff2fc2ac2f5a93d9fb557dfda83fe3614b9
Omeiza Alabi <omeizaalabi@gmail.com>
2023-01-24 11:34:58 +0100

    Audit trail

---
+++ lmis_cms/src/api/data-insight/content-types/data-insight/lifecycles.js
@@ -0,0 +1,74 @@
+module.exports = {
+  afterCreate(event) {
+    const { result, params } = event;
+    strapi.entityService.create("api::audit-trail.audit-trail", {
+      data: {
+        contentType: event.model.tableName,
+        action: "Create Content",
+        content: result,
+        author: result.createdBy,
+        authorEmail: result.createdBy.email,
+        params: params,
+        request: event,
+      },
+    });
+  },
+
+  afterUpdate(event) {
+    const { result, params } = event;
+    strapi.entityService.create("api::audit-trail.audit-trail", {
+      data: {
+        contentType: event.model.tableName,
+        action: "Update Content",
+        content: result,
+        author: result.createdBy,
+        authorEmail: result.createdBy.email,
+        params: params,
+        request: event,
+      },
+    });
+  },
+
+  afterDelete(event) {
+    const { result, params } = event;
+    strapi.entityService.create("api::audit-trail.audit-trail", {
+      data: {
+        contentType: event.model.tableName,
+        action: "Delete Content",
+        content: result,
+        author: result.createdBy,
+        authorEmail: result.createdBy.email,
+        params: params,
+        request: event,
+      },
+    });
+  },
+
+  // afterFindOne(event) {
+  //   const { result, params } = event;
+  //   strapi.entityService.create("api::audit-trail.audit-trail", {
+  //     data: {
+  //       contentType: "Indicator",
+  //       action: "Single Content Search",
+  //       content: result.Content,
+  //       author: result.createdBy, authorEmail: result.createdBy.email,
+  //       params: params,
+  //       request: event,
+  //     },
+  //   });
+  // },
+
+  // afterFindMany(event) {
+  //   const { result, params } = event;
+  //   strapi.entityService.create("api::audit-trail.audit-trail", {
+  //     data: {
+  //       contentType: "Indicator",
+  //       action: "Multiple Content Search",
+  //       content: result.Content,
+  //       author: result.createdBy, authorEmail: result.createdBy.email,
+  //       params: params,
+  //       request: event,
+  //     },
+  //   });
+  // },
+};
