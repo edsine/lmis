0bd4c76d2ca81fed3032a3c37ba4757e0d88b343
Adewale Samuel <samadewale2014@gmail.com>
2023-03-06 13:58:41 +0100

    New features on Strapi Backend - on Event Page

---
+++ lmis_cms/src/api/event-comment/controllers/event-comment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * event-comment controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::event-comment.event-comment');
---
+++ lmis_cms/src/api/event-comment/routes/event-comment.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * event-comment router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::event-comment.event-comment');
